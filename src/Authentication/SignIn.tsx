3a8d8a9837e0b7486420e5a713e51a2b2a8817c3
Cushione <maulerovak@gmail.com>
2023-02-26 14:39:55 +0000

    Add error message to sign in form

--- src/Authentication/SignIn.tsx
+++ src/Authentication/SignIn.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { MouseEvent, useState } from 'react'
+import Alert from 'react-bootstrap/esm/Alert'
 import Button from 'react-bootstrap/esm/Button'
 import Form from 'react-bootstrap/esm/Form'
 
@@ -12,22 +13,31 @@ function SignIn() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [rememberMe, setRememberMe] = useState(true)
+  const [error, setError] = useState<string | undefined>()
 
   const handleSubmit = (event: MouseEvent): void => {
     event.preventDefault()
-    localStorage.setItem("RememberMe", JSON.stringify(rememberMe))
-    axios.post<SignInResponse>(
-      import.meta.env.VITE_API_URL + 'api/token/',
-      {
-        username,
-        password,
-      },
-      {retry: false} as any
-    )
+    setError(undefined)
+    localStorage.setItem('RememberMe', JSON.stringify(rememberMe))
+    axios
+      .post<SignInResponse>(
+        import.meta.env.VITE_API_URL + 'api/token/',
+        {
+          username,
+          password,
+        },
+        { retry: false } as any
+      )
+      .catch((error) => {
+        if (error.response) {
+          setError(error.response.data.detail)
+        }
+      })
   }
 
   return (
     <Form>
+      {error && <Alert variant={'danger'}>{error}</Alert>}
       <Form.Group className='mb-3' controlId='signInUsername'>
         <Form.Label>Username</Form.Label>
         <Form.Control
