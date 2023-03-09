51dcc576e3fdd47824353825a8cf427687a140b6
Cushione <maulerovak@gmail.com>
2023-03-09 10:56:36 +0000

    Update error page

--- src/Components/ErrorPage/ErrorPage.tsx
+++ src/Components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link, useRouteError } from 'react-router-dom'
+import { Link, useNavigate, useRouteError } from 'react-router-dom'
 import './ErrorPage.scss'
 
 export default function ErrorPage() {
   const error = useRouteError()
-  console.error(error)
+  const navigate = useNavigate()
 
   return (
     <div id='error-page'>
@@ -13,7 +13,13 @@ export default function ErrorPage() {
       <p className='mb-5'>
         Error: <i>{error.statusText || error.message}</i>
       </p>
-      <p>Would you like to return to the <Link to={'/'}>Homepage</Link>?</p>
+      <p>
+        Would you like to&nbsp;
+         <a href='javascript:void 0' onClick={() => navigate(-1)}>
+          Go Back
+        </a>
+        &nbsp;or return to the <Link to={'/'}>Homepage</Link>?
+      </p>
     </div>
   )
 }
